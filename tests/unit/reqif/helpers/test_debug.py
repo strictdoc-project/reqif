65c71af7ba510c7f782a825b180a434e6508aaa5
Stanislav Pankevich <s.pankevich@gmail.com>
2022-12-25 14:42:18 +0100

    helpers/debug: auto-repr using decorators, with parameter

--- tests/unit/reqif/helpers/test_debug.py
+++ tests/unit/reqif/helpers/test_debug.py
@@ -1,29 +1,31 @@
 from reqif.helpers.debug import auto_described
 
 
-@auto_described
-class A:
-    def __init__(self):
-        self.prop1 = "FOO"
-        self.prop2 = None
-        self.prop3 = [1, 2, 3]
-        self.prop4 = {"a": 123}
-        self.prop5 = bytes("123", "utf-8")
-
-
-@auto_described
-class B:
-    def __init__(self):
-        self.B1 = "FOO"
-        self.B2 = None
-
-
 def test_01_debug() -> None:
+    @auto_described
+    class A:
+        def __init__(self):
+            self.prop1 = "FOO"
+            self.prop2 = None
+            self.prop3 = [1, 2, 3]
+            self.prop4 = {"a": 123}
+            self.prop5 = bytes("123", "utf-8")
+
+    @auto_described
+    class B:
+        def __init__(self):
+            self.B1 = "FOO"
+            self.B2 = None
+
     a = A()
     b = B()
     a.prop2 = b
     b.B2 = a
-    assert (str(a)) == (
+
+    a_str = str(a)
+    a_repr = repr(a)
+
+    assert a_str == (
         "A("
         'prop1 = "FOO", '
         "prop2 = B(...), "
@@ -31,3 +33,28 @@ def test_01_debug() -> None:
         "prop4 = {1 elements}, "
         "prop5 = b'123')"
     )
+    assert a_str == a_repr
+
+
+def test_02_debug() -> None:
+    @auto_described(str_and_repr=True)
+    class A:
+        def __init__(self):
+            self.prop1 = "FOO"
+            self.prop2 = None
+            self.prop3 = [1, 2, 3]
+            self.prop4 = {"a": 123}
+            self.prop5 = bytes("123", "utf-8")
+
+    a = A()
+    a_str = str(a)
+    a_repr = repr(a)
+    assert a_str == (
+        "A("
+        'prop1 = "FOO", '
+        "prop2 = NoneType(...), "
+        "prop3 = [3 elements], "
+        "prop4 = {1 elements}, "
+        "prop5 = b'123')"
+    )
+    assert a_str == a_repr
